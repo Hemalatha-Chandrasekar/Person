63ab8e4f0021125c5ef2da468724b1ede4b4bd90
Hemalatha-Chandrasekar <hema.alliswell@gmail.com>
2024-12-03 14:14:20 -0600

    Inheritance Commit

--- src/Boy.java
+++ src/Boy.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
+public class Boy extends Person {
+    static double ageFactor= 1.1;
+    public String talk() {
+        return(super.talk() + "... but I love java Class.");
+    }
+    public String walk() {
+        return("I am now walking");
+    }
 
-public class Boy {
 }
--- src/Sample_Person.java
+++ src/Sample_Person.java
@@ -1,4 +1,49 @@
-package PACKAGE_NAME;
-
 public class Sample_Person {
+//    String name;
+//    int lifespan;
+//    int age;
+//    int ageFactor;
+//    public Sample_Person() {
+//
+//    }
+//    public Sample_Person(String name) {
+//
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public int getLifespan() {
+//        return lifespan;
+//    }
+//
+//    public int getAge() {
+//        return age;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public void setLifespan(int lifespan) {
+//        this.lifespan = lifespan;
+//    }
+//
+//    public void setAge(int age) {
+//        this.age = age;
+//
+//    }
+//    public String toString() {
+//        return name;
+//    }
+//    public String talk(){
+//        return "I am a person can talk";
+//    }
+//    public String walkk(){
+//        return "I am a person can walk";
+//    }
+//    public static void double lifespan (){
+//        return (lifespan * ageFactor);
+//    }
 }
